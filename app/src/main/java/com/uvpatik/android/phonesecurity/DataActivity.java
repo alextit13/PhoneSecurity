7b12d20cbc8ff0de47677eb293681401f3440d97
Alexey <alexcas@mail.ru>
2018-03-18 18:42:02 +0200

    v 1.1

---
+++ app/src/main/java/com/uvpatik/android/phonesecurity/DataActivity.java
@@ -0,0 +1,42 @@
+package com.uvpatik.android.phonesecurity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.about_application:
+                AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+                dialog
+                        .setTitle(R.string.about_application_item_menu)
+                        .setMessage(R.string.about_programm_message_dialog)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }).show();
+        }
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/uvpatik/android/phonesecurity/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.uvpatik.android.phonesecurity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    nextPointInStack();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void nextPointInStack() {
+        startActivity(new Intent(SplashActivity.this,DataActivity.class));
+    }
+}
